72ffe9b138b84c5755d5a72457bd3004cb57d3f8
OddMagnet <mibruenen@gmail.com>
2022-01-09 10:31:48 +0100

    Add basic game board

--- DiceOff/ContentView.swift
+++ DiceOff/ContentView.swift
@@ -8,9 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var game = Game(rows: 8, columns: 11)
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 5) {
+            Text("Dice Off")
+                .font(.largeTitle.bold())
+
+            HStack(spacing: 20) {
+                Text("Green: \(game.greenScore)")
+                    .foregroundColor(.green)
+                    .font(game.activePlayer == .green ? .title.bold() : .title)
+
+                Text("Red: \(game.redScore)")
+                    .foregroundColor(.red)
+                    .font(game.activePlayer == .red ? .title.bold() : .title)
+            }
+
+            ForEach(game.board.indices, id: \.self) { row in    // go over every row
+                HStack(spacing: 5) {                            // create an HStack for the row
+                    ForEach(game.board[row]) { dice in          // go over every dice in the row
+                        DiceView(dice: dice)                    // create a DiceView for the dice
+                    }
+                }
+            }
+        }
+        .padding(.horizontal)
     }
 }
 
