82a145f3d1d8a2e6739ecb26b43a675ace14b0d4
OddMagnet <mibruenen@gmail.com>
2022-01-09 09:57:25 +0100

    Initial Commit

---
+++ DiceOff/DiceOffApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceOffApp.swift
+//  DiceOff
+//
+//  Created by Michael Brünen on 09.01.22.
+//
+
+import SwiftUI
+
+@main
+struct DiceOffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
